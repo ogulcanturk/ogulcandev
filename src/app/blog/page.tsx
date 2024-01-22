1afe2be802adfc90f31f18cfde7dd2fb75cf3a1e
Ogulcan <account.work@ogulcan.dev>
2024-01-22 16:37:40 +0300

    mdx blog added

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() : React.ReactNode {
+  return (
+    <div className="max-w-xl mx-auto my-24">
+      <div className="">Picture</div>
+      <div className="">Info</div>
+      <div className="">Blog</div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() : React.ReactNode {
+export default function Blog() : React.ReactNode {
   return (
-    <div className="">
-      Home
+    <div className="max-w-xl mx-auto my-24">
+      <div className="">Blog</div>
     </div>
   )
 }
