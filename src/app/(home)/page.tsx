22702778dd82cd03e2e66bf5c978494033619cff
Ogulcan <account.work@ogulcan.dev>
2024-01-24 16:48:15 +0300

    item list added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,10 @@
 // Components
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 
 // Types
 import React from "react"
 
-export default function Home() : React.ReactNode {
+export default function Home(): React.ReactNode {
 
   const posts = [{
     id: 1,
@@ -20,17 +20,22 @@ export default function Home() : React.ReactNode {
 
   return (
     <div className="flex gap-2 mt-10">
-      {posts.map((item, index) => {
-        return (
-          <div className="grid grid-row gap-2">
-            <a href={item.slug} className="flex">
-              <span className="">{item.date}</span>
-              <span className="">{item.title}</span>
-              <span className="">1</span>
-            </a>
-          </div>
-        )
-      })}
+      <ul role="list" className="w-full divide-y divide-gray-100">
+        {posts.map((item: any, index: number) => {
+          return (
+            <li key={index} className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-2 sm:flex-nowrap">
+              <div className="flex items-end gap-2">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  <a href="#" className="hover:underline">{item.title}</a>
+                </p>
+                <p className="text-xs leading-5 text-gray-500">
+                  <time>2d ago</time>
+                </p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
-    )
+  )
 }
