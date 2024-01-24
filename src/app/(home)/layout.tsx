f05c35f90f905c180d8146e10403ea1029e22c2b
Ogulcan <account.work@ogulcan.dev>
2024-01-24 16:33:56 +0300

    test ui

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,14 @@ import React from "react"
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-xl mx-auto my-24">
-      <span className="">Home Layout</span>
+      <div className="flex flex-col gap-2">
+        <h1 className="font-semibold text-xl text-slate-800">
+          Oğulcan Türk
+        </h1>
+        <h2 className="font-normal text-md text-slate-800">
+          I'm a software developer from Türkiye making useful things for the internet and exploring the world.
+        </h2>
+      </div>
       {children}
     </div>
   )
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,10 +5,32 @@ import { Button } from "@/components/ui/button"
 import React from "react"
 
 export default function Home() : React.ReactNode {
+
+  const posts = [{
+    id: 1,
+    date: "Jan 24, 2024",
+    url: "/blog/example-blog-1",
+    title: "Example blog #1"
+  }, {
+    id: 2,
+    date: "Jan 24, 2024",
+    slog: "/blog/example-blog-2",
+    title: "Example blog #2"
+  }]
+
   return (
-    <div className="">
-      <div className="">Home</div>
-      <Button>Example</Button>
+    <div className="flex gap-2 mt-10">
+      {posts.map((item, index) => {
+        return (
+          <div className="grid grid-row gap-2">
+            <a href={item.slug} className="flex">
+              <span className="">{item.date}</span>
+              <span className="">{item.title}</span>
+              <span className="">1</span>
+            </a>
+          </div>
+        )
+      })}
     </div>
     )
 }
