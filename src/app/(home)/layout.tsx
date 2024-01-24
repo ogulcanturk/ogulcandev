7944946fb9ff22cfe856638588b3f36b59267811
Ogulcan <account.work@ogulcan.dev>
2024-01-24 16:59:04 +0300

    bug fix

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,15 +1,15 @@
 // Types
-import React from "react"
+import React from 'react'
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-xl mx-auto my-24">
       <div className="flex flex-col gap-2">
         <h1 className="font-semibold text-xl text-slate-800">
-          Oğulcan Türk
+          Oğulcan
         </h1>
         <h2 className="font-normal text-md text-slate-800">
-          I'm a software developer from Türkiye making useful things for the internet and exploring the world.
+          Im a software developer from Türkiye making useful things for the internet and exploring the world.
         </h2>
       </div>
       {children}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Oğulcan Türk',
-  description: 'DevOps and Code',
+  title: 'Oğulcan',
+  description: 'Im a software developer from Türkiye making useful things for the internet and exploring the world.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
